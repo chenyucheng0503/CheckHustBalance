c9f6cccc4acf320cec7a8d252f6f058fbd5cb3fc
graham <1731354527@qq.com>
2021-01-03 17:45:23 +0800

    修改Readme

--- VPS_check_balance.py
+++ VPS_check_balance.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 def start_selenium():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
